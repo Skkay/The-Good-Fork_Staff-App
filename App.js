d9c846e44499e40a315f38272863494479b2c9d6
Skkay <skkay.thomas@gmail.com>
2021-05-26 18:48:11 +0200

    Update 'App.js' to add ReactNavigation components and 'LoginScreen'

--- App.js
+++ App.js
@@ -1,13 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useMemo, useEffect } from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default function App() {
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import LoginScreen from "./src/screens/LoginScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import { AuthContext } from "./src/components/AuthContext";
+
+const App = () => {
+  const Stack = createStackNavigator();
+  const Drawer = createDrawerNavigator();
+
+  const [isConnected, setConnected] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  const signOut = () => {
+    try {
+      AsyncStorage.removeItem("userToken");
+      AsyncStorage.removeItem("userTokenExp");
+      AsyncStorage.removeItem("userId");
+      AsyncStorage.removeItem("userRoles");
+      setConnected(false);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  const authContext = useMemo(() => ({
+    signOut: () => signOut(),
+  }), []);
+
+  // Try to retrieve stored user token
+  useEffect(() => {
+    setTimeout(async () => {
+      try {
+        const userTokenExp = await AsyncStorage.getItem("userTokenExp");
+        const userToken = await AsyncStorage.getItem("userToken");
+        if (userTokenExp > Math.floor(Date.now() / 1000)) {
+          console.log("Token still valid for", userTokenExp - Math.floor(Date.now() / 1000), "seconds");
+          setConnected(true);
+        }
+        setLoading(false);
+      } catch (e) {
+        console.log(e);
+      }
+    }, 1000);
+  }, []);
+
+  // Loading indicator
+  if (isLoading) {
+    return (
+      <View style={styles.activityIndicator}>
+        <ActivityIndicator size="large" color="#000000" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+        {isConnected ? (
+          <Text>Connected</Text>
+        ) : (
+          <Stack.Navigator initialRouteName="Login">
+            <Stack.Screen name="Login" options={{ title: "Connexion" }}>
+              {({ navigation }) => <LoginScreen navigation={navigation} setConnected={setConnected} />}
+            </Stack.Screen>
+          </Stack.Navigator>
+        )}
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
@@ -19,3 +83,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
