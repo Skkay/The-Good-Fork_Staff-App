bdc06367951a393c9a4b55b58886b1bd768fa1eb
Skkay <skkay.thomas@gmail.com>
2021-05-26 19:10:14 +0200

    Add "Bars" svg icon

---
+++ src/components/svg/Icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import SvgIcon from 'react-native-svg-icon';
+import icons from './icons.js';
+
+const Icon = (props) => <SvgIcon {...props} svgs={icons} />;
+
+export default Icon;
