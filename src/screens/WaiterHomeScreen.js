92463ffe186d673b9f54c179e9caf2444f570cd6
Skkay <skkay.thomas@gmail.com>
2021-05-27 14:38:44 +0200

    Add 'WaiterHomeScreen'
    The home screen for waiters staff

---
+++ src/screens/WaiterHomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { SafeAreaView, Text } from "react-native";
+
+const WaiterHomeScreen = () => {
+  return (
+    <SafeAreaView>
+      <Text>Waiter home screen</Text>
+    </SafeAreaView>
+  );
+};
+
+export default WaiterHomeScreen;
